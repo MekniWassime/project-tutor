2a2c22b29f393446b8cbd681fff12d7198977c47
Oumaima Kboubi <oumaima.kboubi@insat.u-carthage.tn>
2022-02-15 16:34:24 +0000

    show profile birthdate fix

--- project-tutor-frontend/src/app/profile/user-profile/user-profile.component.ts
+++ project-tutor-frontend/src/app/profile/user-profile/user-profile.component.ts
@@ -10,22 +10,22 @@ import { ProfileService } from 'src/app/profile.service';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-  user : Mentor = new Mentor( 1,"foulena ben foulen1", "name.fgahe@gmail.com",new Date("14-01-1999"),98765432,'/assets/img/joe-gardner-2.jpg','passwordii', 'tutor');
+  user : Mentor = new Mentor( 1,"foulena ben foulen1", "name.fgahe@gmail.com",new Date(1999,1,14),98765432,'/assets/img/joe-gardner-2.jpg','passwordii', 'tutor');
   mentor: Mentor | undefined;
   constructor(private authService:AuthService, private profileService:ProfileService) { }
 
 
   async loadUser(): Promise<void> {
     await new Promise(resolve => setTimeout(resolve, 1000));
-    this.user = new User( 1,"foulena ben foulen", "name.fgahe@gmail.com",new Date(1999,1,14,0,0,0,0),98765432,'/assets/img/joe-gardner-2.jpg','passwordii',[]);
+    this.user = new User( 1,"foulena ben foulen", "name.fgahe@gmail.com",new Date(1999,1,14),98765432,'/assets/img/joe-gardner-2.jpg','passwordii',[]);
   }
   async loadMentor(): Promise<void> {
     const mentor = await this.profileService.fetchMentor(9);
     // console.log('information');
     console.log(mentor);
     await new Promise(resolve => setTimeout(resolve, 1000));
-    this.user = new Mentor( 1,"foulena ben foulen1", "name.fgahe@gmail.com",new Date("14-01-1999"),98765432,'/assets/img/joe-gardner-2.jpg','passwordii', 'tutor');
-  } 
+    this.user = new Mentor( 1,"foulena ben foulen1", "name.fgahe@gmail.com",new Date(1999,1,14),98765432,'/assets/img/joe-gardner-2.jpg','passwordii', 'tutor');
+  }
 
   ngOnInit(): void {
     this.loadMentor()
