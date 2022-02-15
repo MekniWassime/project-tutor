3afc81ec0be3cd91698a2034f74f76b0ede2a504
Oumaima Kboubi <oumaima.kboubi@insat.u-carthage.tn>
2022-02-15 16:23:19 +0000

    contact us form

--- project-tutor-frontend/src/app/contact-us/contact-us.component.ts
+++ project-tutor-frontend/src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ContactForm } from '../courses/models/contactForm';
 
 @Component({
   selector: 'app-contact-us',
@@ -8,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class ContactUsComponent implements OnInit {
   focus: any;
   focus1: any;
+
+  contactForm : ContactForm =new ContactForm('','','');
+
+  async loadContact(): Promise<void> {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    this.contactForm= new ContactForm('','','');
+  }
+
+  onSubmit(contactFormulaire: NgForm){
+    console.log(contactFormulaire.value);
+    console.log(contactFormulaire.value['password']);
+    alert("Your message is submited succefully. Thanks for your feedback");
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadContact();
   }
 
 }
